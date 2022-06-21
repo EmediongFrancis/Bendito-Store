0f38b94fe2451e44bfd6014fa2d1694bc66bcdc3
Emediong Francis <emediongfrancis@gmail.com>
2022-06-21 09:20:58 +0100

    Write and test first API route

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+const products = require('./routes/products');
+
+app.use('/api/v1', products);
+
 module.exports = app;
---
+++ backend/controllers/productsController.js
@@ -0,0 +1,7 @@
+
+exports.getProducts = (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        message: 'Products fetched successfully.'
+    });
+}
