0a22e69f03c18e40f6bda4feae9fd4351af741aa
Emediong Francis <emediongfrancis@gmail.com>
2022-06-22 17:12:59 +0100

    Init connection in entry script

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,8 +4,7 @@ const mongoose = require('mongoose');
 const connectDB = () => {
     mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true
+        useUnifiedTopology: true
     }).then(conBool => {
         console.log(`MongoDB Connected: ${conBool.connection.host}`);
     })
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const app = require('./app');
+const connectDB = require('./config/database');
 
 const dotenv = require('dotenv');
 
 dotenv.config({ path: 'backend/config/config.env' });
+
+connectDB();
+
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
 })
