50cdf16ae58fe869259fe6be11f6e72713ef0f02
Emediong Francis <emediongfrancis@gmail.com>
2022-06-24 10:42:00 +0100

    Add route for adding new products

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -2,11 +2,14 @@
 const express = require('express');
 const router = express.Router();
 
-// Import `getProducts` function from `productsController.js`.
-const { getProducts } = require('../controllers/productsController');
+// Import `getProducts` and `newProduct` function from `productsController.js`.
+const { getProducts, newProduct } = require('../controllers/productsController');
 
 // Create route for `/products` endpoint.
 router.route('/products').get(getProducts);
+
+// Create route to add new products.
+router.route('/products/new').post(newProduct);
 
 // Export router object of express.
 module.exports = router;
