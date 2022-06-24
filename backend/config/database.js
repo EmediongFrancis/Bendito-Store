ea8b666c0c63d722e1abfc8c2b31531f1c4ee2b7
Emediong Francis <emediongfrancis@gmail.com>
2022-06-24 10:05:18 +0100

    Comment out code for easy readability

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,7 @@
+// Import mongoose for MongoDB.
 const mongoose = require('mongoose');
 
-
+// Write DB connection logic.
 const connectDB = () => {
     mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
@@ -10,5 +11,5 @@ const connectDB = () => {
     })
 }
 
-
+// Export connection logic to be used externally.
 module.exports = connectDB;
