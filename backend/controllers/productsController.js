80cb44472480dcdb22667a31576d4b70185b44e6
Emediong Francis <emediongfrancis@gmail.com>
2022-06-25 12:28:52 +0100

    Convert :id to ObjectId hex format for proper error parsing

--- backend/controllers/productsController.js
+++ backend/controllers/productsController.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const ObjectId = require('mongodb').ObjectId;
 
 // Create new product using product model.
 exports.addProduct = async (req, res, next) => {
@@ -23,7 +24,7 @@ exports.getProducts = async (req, res, next) => {
 
 // Get single product by ID.
 exports.getProduct = async (req, res, next) => {
-    const product = await Product.findById(req.params.id);
+    const product = await Product.findById(ObjectId(req.params.id));
 
     if (!product) {
         return res.status(404).json({
