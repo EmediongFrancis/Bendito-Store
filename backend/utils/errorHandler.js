b0552fa0efd5ff978660b56972a9d3c7969dd90f
Emediong Francis <emediongfrancis@gmail.com>
2022-06-25 14:28:57 +0100

    Instantiate error handler

---
+++ backend/utils/errorHandler.js
@@ -0,0 +1,11 @@
+// Instantiates error handler.
+class Errorhandler extends Error {
+    constructor(message, statusCode) {
+        super(message);
+        this.statusCode = statusCode;
+
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+module.exports = Errorhandler;
