3f6ebfccd1eafe80971f3e699fd84161258b2f1e
Emediong Francis <emediongfrancis@gmail.com>
2022-06-25 11:47:29 +0100

    Create route to fetch single product by ID

--- backend/controllers/productsController.js
+++ backend/controllers/productsController.js
@@ -20,3 +20,20 @@ exports.getProducts = async (req, res, next) => {
         products
     });
 }
+
+// Get single product by ID.
+exports.getProduct = async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+    
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found.'
+        });
+    }
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+}
