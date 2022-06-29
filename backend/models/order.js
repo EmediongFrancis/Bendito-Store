0cd8996664eeae13de3606a8b63a5de9fff9e7c2
Emediong Francis <emediongfrancis@gmail.com>
2022-06-29 13:39:46 +0100

    Create orders schema

---
+++ backend/models/order.js
@@ -0,0 +1,116 @@
+const mongoose = require('mongoose');
+
+// Define order schema.
+const orderSchema = new mongoose.Schema({
+    shippingInfo: {
+
+        type: String,
+        required: true,
+
+        address: {
+            type: String,
+            required: true
+        },
+        city: {
+            type: String,
+            required: true
+        },
+        phone: {
+            type: String,
+            required: true
+        },
+        postalCode: {
+            type: String,
+            required: true
+        },
+        country: {
+            type: String,
+            required: true
+        }
+    },
+
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+
+    orderItems: [{
+        name: {
+            type: String,
+            required: true
+        },
+        quantity: {
+            type: Number,
+            required: true
+        },
+        image: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: true
+        },
+        productRef: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Product',
+            required: true
+        }
+    }],
+
+    paymentInfo: {
+        id: {
+            type: String
+        },
+        status: {
+            type: String
+        }
+    },
+
+    paidAt: {
+        type: Date
+    },
+
+    itemsPrice: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+
+    taxPrice: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+
+    shippingPrice: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+
+    totalPrice: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+
+    orderStatus: {
+        type: String,
+        required: true,
+        default: 'Processing'
+    },
+
+    deliveryDate: {
+        type: Date
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+module.exports = mongoose.model('Order', orderSchema);
