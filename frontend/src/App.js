905f06620a25cb29cd540c955f2c9ca79cdfd6f3
Emediong Francis <emediongfrancis@gmail.com>
2022-07-04 13:37:54 +0000

    Add dummy product to homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <HomePage />
       <Footer />
     </div>
   );
--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -3,35 +3,35 @@ import React from 'react'
 const HomePage = () => {
   return (
     <div className='container container-fluid'>
-        <h1 id="products_heading">Latest Products</h1>
+      <h1 id="products_heading">Latest Products</h1>
 
-        <section id="products" class="container mt-5">
-      <div class="row">
-        <div class="col-sm-12 col-md-6 col-lg-3 my-3">
-          <div class="card p-3 rounded">
-            <img
-              alt='product'
-              class="card-img-top mx-auto"
-              src="https://m.media-amazon.com/images/I/617NtexaW2L._AC_UY218_.jpg"
-            />
-            <div class="card-body d-flex flex-column">
-              <h5 class="card-title">
-                <a href="dddf">128GB Solid Storage Memory card - SanDisk Ultra</a>
-              </h5>
-              <div class="ratings mt-auto">
-                <div class="rating-outer">
-                  <div class="rating-inner"></div>
+      <section id="products" className="container mt-5">
+        <div className="row">
+          <div className="col-sm-12 col-md-6 col-lg-3 my-3">
+            <div className="card p-3 rounded">
+              <img
+                alt='product'
+                className="card-img-top mx-auto"
+                src="https://m.media-amazon.com/images/I/617NtexaW2L._AC_UY218_.jpg"
+              />
+              <div className="card-body d-flex flex-column">
+                <h5 className="card-title">
+                  <a href="dddf">128GB Solid Storage Memory card - SanDisk Ultra</a>
+                </h5>
+                <div className="ratings mt-auto">
+                  <div className="rating-outer">
+                    <div className="rating-inner"></div>
+                  </div>
+                  <span id="no_of_reviews">(5 Reviews)</span>
                 </div>
-                <span id="no_of_reviews">(5 Reviews)</span>
+                <p className="card-text">$45.67</p>
+                <a href="sddad" id="view_btn" className="btn btn-block">View Details</a>
               </div>
-              <p class="card-text">$45.67</p>
-              <a href="sddad" id="view_btn" class="btn btn-block">View Details</a>
             </div>
           </div>
-        </div>
 
-      </div>
-    </section>
+        </div>
+      </section>
 
     </div>
   )
