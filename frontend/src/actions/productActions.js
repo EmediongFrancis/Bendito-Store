523e3ed01d0dd3630bbcb6260c577b9d899e9b24
Emediong Francis <emediongfrancis@gmail.com>
2022-07-04 17:38:24 +0000

    Set up product actions

---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+import {
+    ALL_PRODUCT_REQUESTS,
+    ALL_PRODUCT_SUCCESS,
+    ALL_PRODUCT_FAIL,
+    CLEAR_ERRORS,
+} from '../constants/productConstants';
+
+export const getProducts = () => async (dispatch) => {
+    try {
+        dispatch({ type: ALL_PRODUCT_REQUESTS });
+        const { data } = await axios.get('/api/v1/products');
+        dispatch({ type: ALL_PRODUCT_SUCCESS, payload: data });
+    } catch (error) {
+        dispatch({ type: ALL_PRODUCT_FAIL, payload: error.response.data.message });
+    }
+}
+
+// Clear errors.
+export const clearErrors = () => async (dispatch) => {
+    dispatch({ type: CLEAR_ERRORS });
+}
