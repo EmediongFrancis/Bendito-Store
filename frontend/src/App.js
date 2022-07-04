c3df4969503f369f431db99918589f6f57ff4278
Emediong Francis <emediongfrancis@gmail.com>
2022-07-04 15:13:29 +0000

    Implement React router for homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
@@ -6,9 +7,15 @@ import HomePage from './components/HomePage';
 function App() {
   return (
     <div className="App">
-      <Header />
-      <HomePage />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <div className="container container-fluid">
+          <Routes>
+            <Route path="/" element={<HomePage />} exact />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter >
     </div>
   );
 }
