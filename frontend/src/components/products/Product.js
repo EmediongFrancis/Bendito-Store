1b89953d459c0ae6132e754d6256b9ed80481800
Emediong Francis <emediongfrancis@gmail.com>
2022-07-05 15:34:00 +0100

    Fix rating stars

--- frontend/src/components/products/Product.js
+++ frontend/src/components/products/Product.js
@@ -16,10 +16,7 @@ const Product = ({ product }) => {
                     </h5>
                     <div className="ratings mt-auto">
                         <div className="rating-outer">
-                            <div className="rating-inner" style={{
-                                width: `${(product.ratings / 5) * 100
-                                    }%`
-                            }}></div>
+                            <div className="rating-inner" style={{ width: `${(product.ratings / 5) * 100}%` }}></div>
                         </div>
                         <span id="no_of_reviews">({product.totalReviews} Reviews)</span>
                     </div>
